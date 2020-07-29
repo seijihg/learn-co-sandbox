a676b4e954f64fe9897da1aa577e182d3abba767
Le Hoang Ngo <seiji@o2.pl>
2020-07-29 16:40:19 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,11 @@
+arr = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+	puts "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(arr)
+  
+  while arr
+
+end
